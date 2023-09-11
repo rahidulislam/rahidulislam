f094c8fa8275abd35ff5d55950c2589471bf95f7
Rahidul Islam <rahiseli@outlook.com>
2023-09-11 21:12:16 +0600

    contact is updated

--- home/models.py
+++ home/models.py
@@ -7,4 +7,21 @@ class SocialMedia(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+# about section
+# Personal Info-> profession, short_desc, desc, dob, phone, email, is_available, city, website, degree, age
+# skill-> name, percentage
+# interest -> interest name, icon name
+# testimonial -> client name, designation, review, client image
+
+#resume section
+# education-> degree name, duration, details, institution_name, address
+# experience -> designation, duration, details, company_name, address
+# experience content-> company, bullet_point
+
+# service
+#service -> name, details, icon_name
+
+# portfolio
+# category -> category_name
+# project -> project name, short_dec, image, url, client name, project date, project image for slider
